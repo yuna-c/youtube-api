a40ec5ba8ecc039bd0eeb7ea08e3051bef318c75
yuna-c <yuna744@gmail.com>
2023-11-17 13:04:22 +0900

    1117 객체 뽑기

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const api_key = 'AIzaSyBgRldfomRBMNoipsSTKYAmfOarH1iIu8o';
 const playList_id = 'PL_gXk6OSOQ5LVWytUDP2MgKhA1-A5h1TJ';
 const base_url = 'https://www.googleapis.com/youtube/v3/playlistItems';
-const result_url = `${base_url}?part=snippet&&api_key=${api_key}&&playlistId=${playList_id}&&maxResults=5`;
+const result_url = `${base_url}?part=snippet&&key=${api_key}&&playlistId=${playList_id}&&maxResults=5`;
 /* 
 브라우저 URL을 통해서 데이터 요청 방식 GET 방식
 요청 URL : 기본값URL?name=value&name=value
@@ -10,4 +10,8 @@ https://console.cloud.google.com/apis/library/youtube.googleapis.com?hl=ko&proje
 https://console.cloud.google.com/apis/credentials?hl=ko&project=redsunmap-1569760786189
 https://developers.google.com/youtube/v3/docs/playlistItems/list?hl=ko
 */
-console.log(result_url);
+// console.log(result_url);
+
+fetch(result_url)
+	.then((data) => data.json)
+	.then((json) => console.log(json));
